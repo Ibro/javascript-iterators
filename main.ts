1a035b228f42cc1229ee8b1964537bfa2cb24220
Ibro <ibro@ibro.me>
2017-04-07 17:22:05 +0200

    using built-in iterator

--- main.ts
+++ main.ts
@@ -16,3 +16,16 @@ let iterator = makeIterator(['coding', 'blast']);
 console.log(iterator.next()); // [object Object] { done: false, value: "coding" }
 console.log(iterator.next()); // [object Object] { done: false, value: "blast" }
 console.log(iterator.next()); // [object Object] { done: true }
+
+
+let numbers = [1, 2, 3];
+let numbersIterator = numbers[Symbol.iterator]();
+
+console.log(numbersIterator.next()); // {value: 1, done: false}
+console.log(numbersIterator.next()); // {value: 2, done: false}
+console.log(numbersIterator.next()); // {value: 3, done: false}
+console.log(numbersIterator.next()); // {value: undefined, done: true}
+
+for (let num of numbers) {
+    console.log(num);
+}
